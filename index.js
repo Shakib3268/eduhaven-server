d087fd1163e8d6f1e094e8a830eff803ac824e2d
Shakib AL Nur <shakibalnur3268@gmail.com>
2023-07-24 22:42:16 +0600

    all

--- index.js
+++ index.js
@@ -25,11 +25,62 @@ async function run() {
     await client.connect();
 
     const usersCollection = client.db("eduDb").collection("college")
+    const collegeCollection = client.db("eduDb").collection("collection")
 
     app.get('/college', async (req, res) => {
         const result = await usersCollection.find().toArray();
         res.send(result);
     })
+
+    app.get('/allcollege/:id',async(req,res) => {
+      const id = req.params.id;
+      const query = ({_id : new ObjectId(id)})
+      const result = await usersCollection.findOne(query)
+      res.send(result)
+    })
+    app.get('/admission/:id',async(req,res) => {
+      const id = req.params.id;
+      const query = ({_id : new ObjectId(id)})
+      const result = await usersCollection.findOne(query)
+      res.send(result)
+    })
+     app.post("/addcollege",async (req,res) => {
+        const body = req.body
+        if(!body){
+            return res.status(404).send({message: "body data not found"})
+        }
+        const result = await collegeCollection.insertOne(body)
+        res.send(result)
+    })
+
+    // app.get("/mycollege",async (req,res) =>{
+    //   console.log(req.query.email)
+    //   let query = {}
+    //   if(req.query?.email){
+    //     query = {email: req.query.email}
+    //   }
+    //   const college = await collegeCollection.find(query).toArray()
+    //   res.send(college)
+    // })
+    app.get('/admitCollage/:email', async (req, res) => {
+      const email = req.params.email;
+      const result = await collegeCollection.find({ email: email }).toArray();
+      res.send(result);
+  })
+  app.get("/searchcollage/:text", async (req, res) => {
+    const text = req.params.text;
+    const result = await usersCollection
+        .find({
+            $or: [
+                {
+                    collagename: { $regex: text, $options: "i" }
+                },
+
+            ],
+        })
+        .toArray();
+    res.send(result);
+});
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
